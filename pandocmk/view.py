8119519eb88967ea2f0754840a7ae21f99cfd0a6
Sergio Correia <sergio.correia@gmail.com>
2019-12-16 05:38:25 -0500

    Update view.py

--- pandocmk/view.py
+++ pandocmk/view.py
@@ -7,7 +7,8 @@ Code for opening external viewers (SumatraPDF)
 # Imports
 # ---------------------------
 
-import subprocess # import Popen, PIPE
+#import subprocess # import Popen, PIPE
+import webbrowser
 
 
 # ---------------------------
@@ -15,6 +16,11 @@ import subprocess # import Popen, PIPE
 # ---------------------------
 
 def run_viewer(pdf_fn, verbose=False):
+	# https://stackoverflow.com/a/15055133
+	webbrowser.open(str(pdf_fn))
+
+
+def old_run_viewer(pdf_fn, verbose=False):
     cmd = ['SumatraPDF.exe', str(pdf_fn), '-reuse-instance']
     if verbose:
     	print("[CMD]", ' '.join(cmd))
